ae8cad23287da66c0fb953c48b172742c095bed8
Alexander Schübl <as333@hdm-stuttgart.de>
2018-12-19 02:53:03 +0100

    Updates

---
+++ precache-manifest.c9e52b342546f7b0046a1093c737c2c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a14038fcbf2af5341d4",
+    "url": "/static/js/main.2a14038f.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "2a14038fcbf2af5341d4",
+    "url": "/static/css/main.b938244d.chunk.css"
+  },
+  {
+    "revision": "067546025799a190584e765cf587aabf",
+    "url": "/index.html"
+  }
+];
